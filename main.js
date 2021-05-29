989730164d8ab45490efcd2dd01980601c646731
Vishnu-Sudhakar <vishnusudhakar097@gmail.com>
2021-05-29 20:52:19 +0530

    ++added longitude

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var str = "Thrissur";
 var data;
+var long;
 var lat;
 var main = document.querySelector("#name");
+var main2 = document.querySelector("#name2");
 function setbg(color) {
   document.getElementById("input").style.background = color;
 }
@@ -10,8 +12,10 @@ async function funcName(link) {
   const response = await fetch(link);
   data = await response.json();
   lat = data.results[0].coordinates.latitude;
+  long = data.results[0].coordinates.longitude;
   console.log(lat);
-  main.innerHTML = lat;
+  main.innerHTML = lat + "is your latitude";
+  main2.innerHTML = long + "is your longitude";
 }
 
 var button = document.querySelector("button");
