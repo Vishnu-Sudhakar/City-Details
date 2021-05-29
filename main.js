66c8a547163c32292aa1c4bcf943185749bb2f30
Vishnu-Sudhakar <vishnusudhakar097@gmail.com>
2021-05-30 03:52:32 +0530

    ++ added Enter to submit, ++first letter of the city will auto capitalize

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var str = "Thrissur";
 var data;
 var long;
 var lat;
+var name;
 var main = document.querySelector("#name");
 var main2 = document.querySelector("#name2");
 function setbg(color) {
@@ -14,13 +15,21 @@ async function funcName(link) {
   lat = data.results[0].coordinates.latitude;
   long = data.results[0].coordinates.longitude;
   console.log(lat);
-  main.innerHTML = lat + "is your latitude";
-  main2.innerHTML = long + "is your longitude";
+  main.innerHTML = lat + " is " + str + "'s latitude";
+  main2.innerHTML = long + " is " + str + "'s longitude";
 }
 
 var button = document.querySelector("button");
 function fn1() {
   str = document.getElementById("input").value;
+  str = str.charAt(0).toUpperCase() + str.slice(1);
   var link = "https://api.openaq.org/v1/latest?city=" + str;
   funcName(link);
 }
+var input = document.getElementById("input");
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("button").click();
+  }
+});
